ef1489b4cca77400f81e1db77200d59ad4dcab3c
AlaaRaedDawood <you@example.com>
2021-02-06 14:58:49 +0200

    fix CORS issues

--- src/server.js
+++ src/server.js
@@ -51,6 +51,19 @@ io.on('connection', socket => {
 app.use((req, res, next) => {
 	req.io = io
 	req.connectUsers = connectUsers
+	// Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://mern-sport-frontend.herokuapp.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+	
 	// console.log("i am here ");
 	return next()
 })
