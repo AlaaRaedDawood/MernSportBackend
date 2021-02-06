ec3e55ff06a0fc12c5a7b61fdc02cde35b9f9ea5
AlaaRaedDawood <you@example.com>
2021-02-06 15:44:34 +0200

    fix issue cors

--- src/server.js
+++ src/server.js
@@ -51,10 +51,10 @@ app.use(cors())
 app.use((req, res, next) => {
 	req.io = io
 	req.connectUsers = connectUsers
-	res.setHeader('Access-Control-Allow-Origin', 'https://mern-sport-frontend.herokuapp.com');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+	// res.setHeader('Access-Control-Allow-Origin', 'https://mern-sport-frontend.herokuapp.com');
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // res.setHeader('Access-Control-Allow-Credentials', true);
 
 	return next()
 })
